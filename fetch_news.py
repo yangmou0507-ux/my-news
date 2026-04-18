362992185eaa6c5404c088e710c7818f840f3894
yangmou0507-ux <yangmou0507@gmail.com>
2026-04-18 20:04:00 +0800

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -1,7 +1,11 @@
 import feedparser
 
 def get_news():
-    # 这个链接就是把推特变成网页能读的格式
-    url = "https://rsshub.app/twitter/user/prettybusy"
+    # 我们换一个更快的“搬运桥梁”地址
+    url = "https://rss.dragonegg.moe/twitter/user/prettybusy"
     data = feedparser.parse(url)
-    return data.entries[:5] # 只拿最近5条
+    
+    # 打印一下看看有没有拿到东西（这是给后台看的）
+    if not data.entries:
+        return []
+    return data.entries[:5]
