e0f20dd53b789d7755be3e29b39881491e858b27
yangmou0507-ux <yangmou0507@gmail.com>
2026-04-18 22:41:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import streamlit as st
 
-st.set_page_config(page_title="PrettyBusy 情报站", page_icon="🦋")
+st.set_page_config(page_title="PrettyBusy 情报站")
 
 st.title("🦋 PrettyBusy 官方情报站")
-st.write("不必翻墙，搬运自X：@Prettybusy_KR！")
+st.write("不必翻墙，同步感受地狱的魅力！")
 
-# --- 第一条消息 ---
 st.divider()
-st.subheader("📅 [4月16日] 贝尔菲格(攻击者) 立绘")
 
-# 这里我直接帮你填好了你刚才拿到的微博链接！
+# 这是你刚才拿到的微博图链，我已经帮你放好了
 st.image("https://wx2.sinaimg.cn/mw2000/009c1GDEly1icba74auw2j30ek0d374x.jpg", caption="贝尔菲格立绘")
 
-st.write("""
-试验1。
-大家不用担心，站长会一直在这里手动同步最新消息的！
-""")
+st.write("贝尔菲格的新立绘公开！这张卡面真的太帅了。")
 
-st.divider()
-st.info("💡 站长提示：本站是站长第一次搭建 可能做的有些简陋😭")
+st.info("💡 如果你能看到这段话，说明我们的网站修好啦！")
